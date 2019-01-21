3ca751c1cb04c47bbfb28a914505fe5b5b53690c
peterforty9 <peter@forty9.co.uk>
2019-01-21 22:36:08 +0000

    Edit description from block

--- docs/js/group-management.js
+++ docs/js/group-management.js
@@ -1,7 +1,8 @@
 ﻿
 $(function () {
     var textbox;
-
+    var hiddenblockdetails;
+   
     //GROUP MANAGEMENT
 
     //Toggle groups
@@ -22,7 +23,13 @@ $(function () {
         var groupnumber = groupstartindex + 1;
         var activitylist = $('#activityrow .cell:nth-child(' + groupnumber + ')');
 
-        if (event.which == 13 && event.ctrlKey) {
+        if (event.shiftKey && event.ctrlKey && event.which == 13) {
+            hiddenblockdetails = $("#blockdetails").hasClass("hidden");
+            $("#blockdetails").removeClass("hidden");
+            $('#blockdetails').focus();
+            event.stopPropagation();
+
+        } else if (event.which == 13 && event.ctrlKey) {
             event.preventDefault();
             appendNewActivity(groupnumber);
             event.stopPropagation();
@@ -64,7 +71,7 @@ $(function () {
     function insertGroup(groupnumber) {
         var grouptextid = Math.random();
         var activitylist = $('#activityrow .cell:nth-child(' + groupnumber + ')');
-        var htmlData = "<div class='grouprelease cell'><div class='group'><div class='grouptext contenteditable='true' id='" + grouptextid + "' data-details=''></div></div></div>";
+        var htmlData = "<div class='grouprelease cell'><div class='group'><div class='grouptext contenteditable='true' id='" + grouptextid + "'></div><input type='hidden' id='" + grouptextid + "-details'></div></div>";
         var activitylisthtml = "<div class='grouprelease cell'><ul class='activities'></ul></div>"
         var groupreleasehtml = "<div class='grouprelease release cell'></div>"
         var group = $("#groups div.cell:nth-child(" + (groupnumber) + ")")
@@ -85,7 +92,7 @@ $(function () {
     //Append new activity
     function appendNewActivity(groupnumber) {
         var activitytextid = Math.random();
-        var htmlData = "<li><div class='activity'><div class='activitytext' contenteditable='true' id='" + activitytextid + "' data-details=''></div></div></li>";
+        var htmlData = "<li><div class='activity'><div class='activitytext' contenteditable='true' id='" + activitytextid + "'></div><input type='hidden' id='" + activitytextid + "-details'></div></li>";
         var activitylist = $('#activityrow .cell:nth-child(' + groupnumber + ') .activities');
 
         //Insert activity 
@@ -104,7 +111,7 @@ $(function () {
     };
 
     // ACTIVITY MANAGEMENT
-    
+
     var storedactivity
     var activityHtml
     var editableactivity
@@ -119,7 +126,13 @@ $(function () {
         var groupstartindex = $(this).parent().parent().parent().parent().index();
         var activitynumber = activitystartindex + 1;
         var groupnumber = groupstartindex + 1;
-        if (event.ctrlKey && event.which == 13) { //Append new story
+        if (event.shiftKey && event.ctrlKey && event.which == 13) {
+            hiddenblockdetails = $("#blockdetails").hasClass("hidden");
+            $("#blockdetails").removeClass("hidden");
+            $('#blockdetails').focus();
+            event.stopPropagation();
+
+        } else if (event.ctrlKey && event.which == 13) { //Append new story
             event.preventDefault();
             appendNewStory(activitynumber, groupnumber);
             event.stopPropagation();
@@ -140,28 +153,28 @@ $(function () {
 
     function insertNewActivity(activitynumber, groupnumber) {
         var activitytextid = Math.random();
-        var htmlData = "<li><div class='activity'><div class='activitytext' contenteditable='true' id='" + activitytextid + "' data-details=''></div></div></li>";
-        var activity = $("#activityrow").find(".cell:nth-child(" + groupnumber + ") .activities li").eq(activitynumber-1 );
-            //Insert activity 
-            $(htmlData).insertAfter($(activity));
-            document.getElementById(activitytextid).focus();
+        var htmlData = "<li><div class='activity'><div class='activitytext' contenteditable='true' id='" + activitytextid + "'></div><input type='hidden' id='" + activitytextid + "-details'></div></li>";
+        var activity = $("#activityrow").find(".cell:nth-child(" + groupnumber + ") .activities li").eq(activitynumber - 1);
+        //Insert activity 
+        $(htmlData).insertAfter($(activity));
+        document.getElementById(activitytextid).focus();
 
-            //Insert new epic
+        //Insert new epic
 
-            var from;
+        var from;
 
-            from = "<div class='epic'><ul class='stories'></ul><div class='addStory'><strong>+</strong></div></div>";
+        from = "<div class='epic'><ul class='stories'></ul><div class='addStory'><strong>+</strong></div></div>";
 
-            $(".releaserow").each(function (index) {
-                $(from).insertAfter($(this).find($(" .cell:nth-child(" + groupnumber + ") .epic:nth-child(" + activitynumber + ")")));
-            });
+        $(".releaserow").each(function (index) {
+            $(from).insertAfter($(this).find($(" .cell:nth-child(" + groupnumber + ") .epic:nth-child(" + activitynumber + ")")));
+        });
 
-        };
+    };
 
     function appendNewStory(activitynumber, groupnumber) {
 
         var storytextid = Math.random()
-        var htmlData = "<li><div class='story'><div class='storytext' contenteditable='true' id='" + storytextid + "' data-details=''></div></div></li>";
+        var htmlData = "<li><div class='story'><div class='storytext' contenteditable='true' id='" + storytextid + "'></div><input type='hidden' id='" + storytextid + "-details'></div></li>";
 
         $(".releaserow").each(function (index) {
             $(htmlData).appendTo($(this).find(" .cell:nth-child(" + groupnumber + ") .epic:nth-child(" + activitynumber + ") .stories"));
@@ -179,13 +192,13 @@ $(function () {
     });
 
     //Check activity li has no text or underlying stories
-    function activityempty(thisObj) {
-        var activitystartindex = thisObj.index();
-        var groupstartindex = thisObj.parent().parent().index();
+    function activityempty(activityline) {
+        var activitystartindex = activityline.index();
+        var groupstartindex = activityline.parent().parent().index();
         var activityindex = activitystartindex + 1;
         var groupnumber = groupstartindex + 1;
 
-        var activityempty = thisObj.children().children().is(':empty');
+        var activityempty = $(activityline).find(" .activity .activitytext").is(':empty');
         var storycount = 0;
 
         //See if any stories exist in any of the releases
@@ -203,34 +216,34 @@ $(function () {
 
     //Remove activity li and underlying epics
     function removeactivity(thisObj) {
-            var activitystartindex = thisObj.index();
-            var groupstartindex = thisObj.parent().parent().index();
-            var activitynumber = activitystartindex + 1;
-            var groupnumber = groupstartindex + 1;
+        var activitystartindex = thisObj.index();
+        var groupstartindex = thisObj.parent().parent().index();
+        var activitynumber = activitystartindex + 1;
+        var groupnumber = groupstartindex + 1;
 
-            //Remove activity
-            thisObj.remove();
+        //Remove activity
+        thisObj.remove();
 
-            //Remove epics
-            $(".releaserow").each(function (index) {
+        //Remove epics
+        $(".releaserow").each(function (index) {
 
-                $(this).find(" .cell:nth-child(" + groupnumber + ") .epic:nth-child(" + activitynumber + ")").remove();
+            $(this).find(" .cell:nth-child(" + groupnumber + ") .epic:nth-child(" + activitynumber + ")").remove();
 
-            });
+        });
     };
 
     //STORY MANAGEMENT
-   
-        var storedStory
-        var storyHtml
-        var editableStory
-        var newStory
-        var newStoryText
+
+    var storedStory
+    var storyHtml
+    var editableStory
+    var newStory
+    var newStoryText
 
     //Create new story when addStory clicked
     $(document).on("click", ".addStory", function () {
         var storytextid = Math.random()
-        var htmlData = "<li><div class='story'><div class='storytext' contenteditable='true' id='" + storytextid + "' data-details=''></div></div></li>";
+        var htmlData = "<li><div class='story'><div class='storytext' contenteditable='true' id='" + storytextid + "'></div><input type='hidden' id='" + storytextid + "-details'></div></li>";
 
         if (($(this).parent().find("li .story").length > 0)) {
             $(htmlData).insertAfter($(this).parent().find("li:last-child"));
@@ -244,24 +257,30 @@ $(function () {
     });
 
     //Create new story when enter key pressed
-$(document).on("keydown", ".storytext", function (event) {
+    $(document).on("keydown", ".storytext", function (event) {
 
-    //var activity = $(this).parent().parent();
-    var activityindex = $(this).parent().parent().parent().parent().index();
-    var groupindex = $(this).parent().parent().parent().parent().parent().index();
-    var activitynumber = activityindex + 1;
-    var groupnumber = groupindex + 1;
+        //var activity = $(this).parent().parent();
+        var activityindex = $(this).parent().parent().parent().parent().index();
+        var groupindex = $(this).parent().parent().parent().parent().parent().index();
+        var activitynumber = activityindex + 1;
+        var groupnumber = groupindex + 1;
 
-        if (event.shiftKey && event.which == 13) { //Insert new activity
+        if (event.shiftKey && event.ctrlKey && event.which == 13) { //edit details
+            hiddenblockdetails = $("#blockdetails").hasClass("hidden");
+            $("#blockdetails").removeClass("hidden");
+            $('#blockdetails').focus();
+            event.stopPropagation();
+
+        } else if (event.shiftKey && event.which == 13) { //Insert new activity
             event.preventDefault();
             insertNewActivity(activitynumber, groupnumber);
             event.stopPropagation();
-        }else if (event.which == 13) { //Insert story
+        } else if (event.which == 13) { //Insert story
             event.preventDefault();
             if ($(this).is(':empty')) { deletestory($(this)) }
             else {
                 var storytextid = Math.random()
-                var htmlData = "<li><div class='story'><div class='storytext' contenteditable='true' id='" + storytextid + "' data-details=''></div></div></li>";
+                var htmlData = "<li><div class='story'><div class='storytext' contenteditable='true' id='" + storytextid + "' data-details=''></div><input type='hidden' id='" + storytextid + "-details' value=''></div></li>";
                 $(htmlData).insertAfter($(this).parent().parent());
                 document.getElementById(storytextid).focus();
                 //document.getElementById(storytextid).removeAttribute("id");
@@ -288,20 +307,44 @@ $(document).on("keydown", ".storytext", function (event) {
     $(document).on("focus", ".storytext, .activitytext, .grouptext", function (event) {
         var currentText = $(this).text();
         $("#blockname").text(currentText);
-        var detailsText = $(this).data("details");
+
+        var detailsText = $(this).next().val();
         textbox = $(this).attr("id");
+        var bgcolor = $(this).css('background-color');
         $("#blockdetails").html(detailsText);
-    }); 
+        $("#blockname").css('background-color', bgcolor); //make title colour the same as the block type
+                
+    });
     $(document).on("keyup", ".storytext, .activitytext, .grouptext", function (event) {
         var currentText = $(this).text();
         $("#blockname").text(currentText);
-     });
+    });
     $(document).on("keyup", "#blockdetails", function (event) {
         var detailsText = $("#blockdetails").html();
+        var hiddeninput = textbox + "-details"
+        var element2 = document.getElementById(hiddeninput);
+        $(element2).val(detailsText);
 
-        var element = document.getElementById(textbox);
-        $(element).data("details", detailsText);
-       
+    });
+    $(document).on("click", "#blockname", function (event) {
+        $("#blockdetails").toggleClass("hidden");
+        hiddenblockdetails = $("#blockdetails").hasClass("hidden");
+    });
+    $(document).on("focusout", "#blockdetails", function (event) {
+        $("#infobox").removeClass("edit");
+        if (hiddenblockdetails) $("#blockdetails").addClass("hidden");
+    });
+    $(document).on("focus", "#blockdetails", function (event) {
+        $("#infobox").addClass("edit");
+    });
+
+    $(document).on("keydown", "#blockdetails", function (event) {
+
+        if (event.shiftKey && event.ctrlKey && event.which == 13) { //back to home block
+            var element2 = document.getElementById(textbox);
+            $(element2).focus();
+            event.stopPropagation();
+        }
     });
     /*
     $(document).on("focus", "#blockcomments", function (event) {
